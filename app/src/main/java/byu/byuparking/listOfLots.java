ffc1206027dc770c08e53804e0660c5b8aba171d
garretthoffa <hoffa@cs.byu.edu>
2017-10-05 16:28:26 -0600

    first draft parking lots.

--- app/src/main/java/byu/byuparking/listOfLots.java
+++ app/src/main/java/byu/byuparking/listOfLots.java
@@ -14,9 +14,19 @@ public class listOfLots {
     public listOfLots() {
         lots A3 = new lots(100,25,7,7,"A", new LatLng(40.249727,-111.646723));
         lots Y37 = new lots(500,333,7,4,"Y", new LatLng(40.248638,-111.656969));
-        //lots Y37 = new lots(500,333,7,4,"Y", new LatLng(40.248638,-111.656969));
+        lots G26 = new lots(250,3,7,4,"G", new LatLng(40.249179,-111.643874));
+        lots A26 = new lots(125,17,7,4,"A", new LatLng(40.249010,-111.644557));
+        lots VP1 = new lots(130,56,5,11,"V", new LatLng(40.248266,-111.644780));
+        lots U45 = new lots(600,435,5,11,"U", new LatLng(40.257881,-111.657452));
+
+
         parkingList.put("A3",A3);
         parkingList.put("Y37",Y37);
+        parkingList.put("G26",G26);
+        parkingList.put("A26",A26);
+        parkingList.put("VP1",VP1);
+        parkingList.put("U45",U45);
+
     }
 
     public TreeMap<String, lots> getParkingList() {
