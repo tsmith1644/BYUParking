3d98b3fcdb8e9ea1222f1e14c1f7fe57eac8b5b0
Trevor <tsmith111.crr@gmail.com>
2017-10-05 16:00:00 -0600

    Centered Map at BYU

--- app/src/main/java/byu/byuparking/MainActivity.java
+++ app/src/main/java/byu/byuparking/MainActivity.java
@@ -34,8 +34,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng byu = new LatLng(40.2518, -111.6493);
+        float zoomLevel = 14.0f;
+        mMap.addMarker(new MarkerOptions().position(byu).title("Marker in BYU"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(byu, zoomLevel));
     }
 }
