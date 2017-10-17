1a79252e7e632a772098156ad8c7f6b2cafb31e7
garretthoffa <hoffa@cs.byu.edu>
2017-10-17 16:48:54 -0600

    Made radio buttons exclusive

--- app/src/main/java/byu/byuparking/FilterActivity.java
+++ app/src/main/java/byu/byuparking/FilterActivity.java
@@ -2,6 +2,7 @@ package byu.byuparking;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
 
 /**
  * Created by samue on 10/17/2017.
@@ -10,11 +11,20 @@ import android.support.v7.app.AppCompatActivity;
 
 public class FilterActivity extends AppCompatActivity
 {
+    private RadioButton faculty;
+    private RadioButton grad;
+    private RadioButton ugrad;
+    private RadioButton visitor;
+    private RadioButton notPaid;
     @Override
     protected void onCreate(Bundle savedInstanceBundle)
     {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.activity_filter);
+
+
     }
+
+
 
 }
